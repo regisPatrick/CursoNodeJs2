ccb93d8fa6dce9cf335ca2f2a6181c68284f583b
regisPatrick <regis86@hotmail.com>
2020-03-31 03:56:39 -0300

    commit 31-03-2020

---
+++ Test/calc.js
@@ -0,0 +1,7 @@
+var SomaFunc = require("./somar");
+var SubFunc = require("./sub");
+var MultiFunc = require("./multi");
+
+console.log(SomaFunc(1, 2));
+console.log(SubFunc(10, 5));
+console.log(MultiFunc(100, 10));
