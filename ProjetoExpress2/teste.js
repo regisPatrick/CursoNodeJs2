174d99a137d9c3bf42eca5fac6d9bde316be59db
regisPatrick <regis86@hotmail.com>
2020-04-09 19:08:33 -0300

    commit 09-04-2020 2

---
+++ ProjetoExpress2/teste.js
@@ -0,0 +1,52 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('test', 'root', 'root', {
+    host: "localhost",
+    dialect: 'mysql'
+});
+
+// sequelize.authenticate().then(function(){
+//     console.log("Conectado com sucesso!");
+// }).catch(function(erro){
+//     console.log("Falha ao se conectar: " + erro);
+// });
+
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
+});
+
+// Postagem.sync({force: true});
+
+// Postagem.create({
+//     titulo: "NodeJS",
+//     conteudo: "Lorem ipsun loren ipsun loren ipsun loren ipsun"
+// });
+
+const Usuario = sequelize.define('usuarios', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+});
+
+// Usuario.sync({force: true});
+
+Usuario.create({
+    nome: "Regis",
+    sobrenome: "Patrick",
+    idade: 33,
+    email: "regis@email.com"
+});
