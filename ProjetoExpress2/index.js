006520b4b1a0738d45d00267e5b609fdae8ae0da
regisPatrick <regis86@hotmail.com>
2020-04-10 23:39:04 -0300

    commit 10-04-2020

--- ProjetoExpress2/index.js
+++ ProjetoExpress2/index.js
@@ -1,26 +1,46 @@
 const express = require("express");
-
 const app = express();
+const handlebars = require("express-handlebars");
+const Sequelize = require('sequelize');
 
-app.get("/", function (req, res){
-    // res.send("Sejam todos bem-vindos ao meu app!")
-    res.sendFile(__dirname + "/html/index.html");
-});
 
-app.get("/sobre", function (req, res) {
-    // res.send("Minha pagina sobre");
-    res.sendFile(__dirname + "/html/sobre.html");
-});
+// Config
+    // Template Engine
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        app.set('view engine', 'handlebars')
+    // Conexão com o banco de dados MySql    
+    const sequelize = new Sequelize('test', 'root', 'root', {
+        host: "localhost",
+        dialect: 'mysql'
+    });
 
-app.get("/blog", function (req, res) {
-    res.send("Bem-vindo ao meu blog!");
-});
+// Rotas
 
-app.get('/ola/:cargo/:nome/:cor', function(req, res){
-    res.send("<h1>Ola " + req.params.nome + "</h1>" + 
-                "<h2>Seu cargo e: " + req.params.cargo + "</h2>" +
-                "<h3>Sua cor favorita e: " + req.params.cor + "</h3>");
-});
+    app.get('/cad', function(req, res){
+        // res.send('ROTA DE CADASTRO DE POSTS')
+        res.render('formulario')
+    })
+
+    
+// app.get("/", function (req, res){
+//     // res.send("Sejam todos bem-vindos ao meu app!")
+//     res.sendFile(__dirname + "/html/index.html");
+// });
+
+// app.get("/sobre", function (req, res) {
+//     // res.send("Minha pagina sobre");
+//     res.sendFile(__dirname + "/html/sobre.html");
+// });
+
+// app.get("/blog", function (req, res) {
+//     res.send("Bem-vindo ao meu blog!");
+// });
+
+// app.get('/ola/:cargo/:nome/:cor', function(req, res){
+//     res.send("<h1>Ola " + req.params.nome + "</h1>" + 
+//                 "<h2>Seu cargo e: " + req.params.cargo + "</h2>" +
+//                 "<h3>Sua cor favorita e: " + req.params.cor + "</h3>");
+// });
 
 
 app.listen(8080, function (){
