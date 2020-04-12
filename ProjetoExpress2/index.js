06e0583536e9e1214588a55f68d412401a0acc3d
regisPatrick <regis86@hotmail.com>
2020-04-11 22:35:35 -0300

    commit 11-04-2020

--- ProjetoExpress2/index.js
+++ ProjetoExpress2/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
+const bodyParser = require('body-parser');
 const Sequelize = require('sequelize');
 
 
@@ -8,6 +9,9 @@ const Sequelize = require('sequelize');
     // Template Engine
         app.engine('handlebars', handlebars({defaultLayout: 'main'}))
         app.set('view engine', 'handlebars')
+    // Body Parser
+        app.use(bodyParser.urlencoded({extended: false}))
+        app.use(bodyParser.json())
     // Conexão com o banco de dados MySql    
     const sequelize = new Sequelize('test', 'root', 'root', {
         host: "localhost",
@@ -21,6 +25,9 @@ const Sequelize = require('sequelize');
         res.render('formulario')
     })
 
+    app.post('/add', function(req, res){
+        res.send("Texto: " + req.body.titulo + " Conteudo: " + req.body.conteudo);
+    })
     
 // app.get("/", function (req, res){
 //     // res.send("Sejam todos bem-vindos ao meu app!")
