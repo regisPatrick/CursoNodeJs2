e22220036e7e7b28e5aef1567003d3cb2bd8e507
regisPatrick <regis86@hotmail.com>
2020-04-15 23:33:33 -0300

    commit 15-04-2020

--- TrabalhandoComMongo/aprendendo.js
+++ TrabalhandoComMongo/aprendendo.js
@@ -11,3 +11,46 @@ const mongoose = require('mongoose')
     }).catch((err) => {
         console.log("Houve um erro ao se conectar ao mongoDB: " + err)
     })
+
+// Model - Usuarios
+// Definindo o model
+    const UsuarioSchema = mongoose.Schema({
+
+        nome: {
+            type: String,
+            require: true
+        },
+        sobrenome: {
+            type: String,
+            require: true      
+        },
+        email: {
+            type: String,
+            require: true
+        },
+        idade: {
+            type: Number,
+            require: true
+        },
+        pais: {
+            type: String
+        }
+
+    })
+
+// Collection
+    mongoose.model('usuarios', UsuarioSchema)
+
+    const Regis = mongoose.model('usuarios')
+
+    new Regis({
+        nome: "Jhon",
+        sobrenome: "Doe",
+        email: "jhon@email.com",
+        idade: 21,
+        pais: "EUA"
+    }).save().then(() => {
+        console.log("Usuário criado com sucesso!")
+    }).catch((err) => {
+        console.log("Houve um erro ao registrar o usuário: " + err)
+    })
