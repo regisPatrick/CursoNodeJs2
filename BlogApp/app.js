48dba025d8ebfa89d9e93df6fdea1f88e637991d
regisPatrick <regis86@hotmail.com>
2020-04-20 01:19:23 -0300

    commit 20-04-2020

--- BlogApp/app.js
+++ BlogApp/app.js
@@ -6,8 +6,23 @@
     const admin = require('./routes/admin')
     const path = require('path')
     const mongoose = require('mongoose')
+    const session = require('express-session')
+    const flash = require('connect-flash')
 
 // Configurações
+    // Sessão
+        app.use(session({
+            secret: "cursodenode",
+            resave: true,
+            saveUninitialized: true
+        }))
+        app.use(flash())
+    // Middleware
+        app.use((req, res, next) => {
+            res.locals.success_msg = req.flash('success_msg')
+            res.locals.error_msg = req.flash('error_msg')
+            next()
+        })    
     // Body Parser
         app.use(bodyParser.urlencoded({extended: true}))
         app.use(bodyParser.json())
@@ -25,7 +40,12 @@
             console.log('Erro ao se conectar' + err)
         })
     // Public
-        app.use(express.static(path.join(__dirname, 'public')))             
+        app.use(express.static(path.join(__dirname, 'public')))
+        
+        // app.use((req, res, next) => {
+        //     console.log('Oi, eu sou um middleware')
+        //     next()
+        // })
 // Rotas
     app.get('/', (req,res) => {
         res.send('Rota Principal')
--- BlogApp/routes/admin.js
+++ BlogApp/routes/admin.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+require('../models/Categoria')
+const Categoria = mongoose.model('categorias')
 
 
 router.get('/', (req, res) => {
@@ -16,6 +19,19 @@ router.get('/categorias', (req, res) => {
 
 router.get('/categorias/add', (req, res) => {
     res.render('admin/addcategorias')
+})
+
+router.post('/categorias/nova', (req, res) => {
+    const novaCategoria = {
+        nome: req.body.nome,
+        slug: req.body.slug
+    }
+
+    new Categoria(novaCategoria).save().then(() => {
+        console.log('Categoria salva com sucesso!')
+    }).catch((err) => {
+        console.log('Erro ao salvar categoria!')
+    })
 })
 
 router.get('/teste', (req, res) => {
