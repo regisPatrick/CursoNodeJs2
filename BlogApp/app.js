57a11c9d07b3843bbb46aa469d88fab04411b1be
regisPatrick <regis86@hotmail.com>
2020-04-26 15:38:03 -0300

    commit 26-04-2020

--- BlogApp/app.js
+++ BlogApp/app.js
@@ -8,6 +8,8 @@
     const mongoose = require('mongoose')
     const session = require('express-session')
     const flash = require('connect-flash')
+    require('./models/Postagem')
+    const Postagem = mongoose.model('postagens')
 
 // Configurações
     // Sessão
@@ -48,7 +50,17 @@
         // })
 // Rotas
     app.get('/', (req,res) => {
-        res.send('Rota Principal')
+        // res.send('Rota Principal')
+        Postagem.find().lean().populate('categorias').sort({data: "desc"}).then((postagens) => {
+            res.render('index', {postagens: postagens})
+        }).catch((err) => {
+            req.flash('error_msg', 'Houve um erro interno')
+            res.redirect('/404')
+        })
+    })
+
+    app.get('/404', (req, res) => {
+        res.send('Erro 404!')
     })
 
     app.get('/posts', (req, res) => {
