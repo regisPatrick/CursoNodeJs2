966a0e9a9fd81d6bc9ce201997b1e5aeabf0ec7f
regisPatrick <regis86@hotmail.com>
2020-05-01 00:20:47 -0300

    commit 01-05-2020

--- BlogApp/app.js
+++ BlogApp/app.js
@@ -12,6 +12,7 @@
     const Postagem = mongoose.model('postagens')
     require('./models/Categoria')
     const Categoria = mongoose.model('categorias')
+    const usuarios = require('./routes/usuario')
 
 // Configurações
     // Sessão
@@ -112,6 +113,8 @@
     // })
 
     app.use('/admin', admin)
+
+    app.use('/usuarios', usuarios)
 // Outros
 const PORT = 8081
 app.listen(PORT, () => {
