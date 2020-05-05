0a98a53298fe15aec1ce669e3d44ca5bc8e41aea
regisPatrick <regis86@hotmail.com>
2020-05-05 16:07:14 -0300

    commit 05-05-2020

--- BlogApp/routes/admin.js
+++ BlogApp/routes/admin.js
@@ -5,17 +5,18 @@ require('../models/Categoria')
 const Categoria = mongoose.model('categorias')
 require('../models/Postagem')
 const Postagem = mongoose.model('postagens')
+const {eAdmin} = require('../helpers/eAdmin')
 
 
-router.get('/', (req, res) => {
+router.get('/', eAdmin, (req, res) => {
     res.render('admin/index')
 })
 
-router.get('/posts', (req, res) => {
+router.get('/posts', eAdmin, (req, res) => {
     res.send("Página de posts")
 })
 
-router.get('/categorias', (req, res) => {
+router.get('/categorias', eAdmin, (req, res) => {
     Categoria.find().lean().sort({date: 'desc'}).then((categorias) => {
         res.render('admin/categorias', {categorias: categorias})
     }).catch((err) => {
@@ -24,11 +25,11 @@ router.get('/categorias', (req, res) => {
     })
 })
 
-router.get('/categorias/add', (req, res) => {
+router.get('/categorias/add', eAdmin, (req, res) => {
     res.render('admin/addcategorias')
 })
 
-router.post('/categorias/nova', (req, res) => {
+router.post('/categorias/nova', eAdmin, (req, res) => {
 
     var erros = []
 
@@ -64,7 +65,7 @@ router.post('/categorias/nova', (req, res) => {
     }
 })
 
-router.get('/categorias/edit/:id', (req, res) => {
+router.get('/categorias/edit/:id', eAdmin, (req, res) => {
     // res.send('Página de edição de categoria')
     Categoria.findOne({_id:req.params.id}).lean().then((categoria) => {
         res.render('admin/editcategorias', {categoria: categoria})
@@ -74,7 +75,7 @@ router.get('/categorias/edit/:id', (req, res) => {
     })
 })
 
-router.post('/categorias/edit', (req, res) => {
+router.post('/categorias/edit', eAdmin, (req, res) => {
     Categoria.findOne({_id: req.body.id}).then((categoria) => {
         
         categoria.nome = req.body.nome
@@ -94,7 +95,7 @@ router.post('/categorias/edit', (req, res) => {
     })
 })
 
-router.post('/categorias/deletar', (req, res) => {
+router.post('/categorias/deletar', eAdmin, (req, res) => {
     // Categoria.remove({_id: req.body.id}).then(() => { // Método remove foi depreciado
     Categoria.deleteOne({_id: req.body.id}).then(() => {    
         req.flash('success_msg', 'Categoria deletada com sucesso!')
@@ -109,7 +110,7 @@ router.post('/categorias/deletar', (req, res) => {
 //     res.send("Isso é um teste")
 // })
 
-router.get('/postagens', (req, res) => {
+router.get('/postagens', eAdmin, (req, res) => {
 
     Postagem.find().lean().populate('categoria').sort({data: 'desc'}).then((postagens) => {
         res.render('admin/postagens', {postagens: postagens})
@@ -120,7 +121,7 @@ router.get('/postagens', (req, res) => {
 
 })
 
-router.get('/postagens/add', (req, res) => {
+router.get('/postagens/add', eAdmin, (req, res) => {
     Categoria.find().lean().then((categorias) => {
         res.render('admin/addpostagem', {categorias: categorias})
     }).catch((err) => {
@@ -129,7 +130,7 @@ router.get('/postagens/add', (req, res) => {
     })
 })
 
-router.post('/postagens/nova', (req, res) => {
+router.post('/postagens/nova', eAdmin, (req, res) => {
 
     var erros = []
 
@@ -160,7 +161,7 @@ router.post('/postagens/nova', (req, res) => {
 
 })
 
-router.get('/postagens/edit/:id', (req, res) => {
+router.get('/postagens/edit/:id', eAdmin, (req, res) => {
 
     Postagem.findOne({_id: req.params.id}).lean().then((postagem) => {
 
@@ -179,7 +180,7 @@ router.get('/postagens/edit/:id', (req, res) => {
 
 })
 
-router.post('/postagem/edit', (req, res) => {
+router.post('/postagem/edit', eAdmin, (req, res) => {
 
     Postagem.findOne({_id: req.body.id}).then((postagem) => {
 
@@ -204,7 +205,7 @@ router.post('/postagem/edit', (req, res) => {
 
 })
 
-router.get('/postagens/deletar/:id', (req, res) => {
+router.get('/postagens/deletar/:id', eAdmin, (req, res) => {
     Postagem.deleteOne({_id: req.params.id}).then(() => {
         req.flash('success_msg', 'Postagem deletada com sucesso!')
         res.redirect('/admin/postagens')
