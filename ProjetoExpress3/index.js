9a6f33a763fbfbb2a8e354b9019db8f3afee3c3a
regisPatrick <regis86@hotmail.com>
2020-05-14 01:27:17 -0300

    commit 14-05-2020

--- ProjetoExpress3/index.js
+++ ProjetoExpress3/index.js
@@ -13,6 +13,13 @@ app.get("/blog", function(req, res){
     res.send("Bem vindo ao meu blog!")
 });
 
+app.get('/ola/:cargo/:nome/:cor', function(req, res){
+    // res.send(req.params);
+    res.send("<h1>Ola " + req.params.nome + "</h1>" +
+        "<h2>Seu cargo é: " + req.params.cargo + "</h2>" +
+        "<h3>Sua cor favorita é: " + req.params.cor + "</h3>");
+});
+
 
 app.listen(8081, function(){
     console.log('Servidor Rodando na URL http://localhost:8081')
