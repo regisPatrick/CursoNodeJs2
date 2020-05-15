684bda1e73dcac3ff7dc6197ed0c38218605dc5c
regisPatrick <regis86@hotmail.com>
2020-05-15 01:17:11 -0300

    commit 15-05-2020

--- ProjetoExpress3/index.js
+++ ProjetoExpress3/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res){
-    res.send("Seja bem-vindo ao meu app!");
+    res.send("Seja muito bem-vindo ao meu app!");
 });
 
 app.get("/sobre", function(req, res){
