a87d88103d2560e65af6a41bb1ae1740f53ce888
regisPatrick <regis86@hotmail.com>
2020-05-20 23:59:27 -0300

    commit 20-05-2020

--- ProjetoExpress3/teste.js
+++ ProjetoExpress3/teste.js
@@ -9,3 +9,43 @@ const sequelize = new Sequelize('test1', 'root', 'root', {
 // }).catch(function(erro){
 //     console.log("Falha ao se conectar: " + erro)
 // })
+
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
+})
+
+// Postagem.sync({force: true})
+
+// Postagem.create({
+//     titulo: "Um Titulo Qualquer",
+//     conteudo: "adsadsdadsdsdasdas"
+// })
+
+const Usuario = sequelize.define('usuarios', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING      
+    }
+})
+
+// Usuario.sync({force: true})
+
+// Usuario.create({
+//     nome: "Regis",
+//     sobrenome: "Patrick",
+//     idade: 99,
+//     email: "regis@email.com"
+// })
