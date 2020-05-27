0041e6b613cd5e642149c166a3c09f5625e33bd7
regisPatrick <regis86@hotmail.com>
2020-05-26 23:34:35 -0300

    commit 26-05-2020 2

--- ProjetoExpress3/index.js
+++ ProjetoExpress3/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
+const Post = require('./models/Post')
 
 // Config
     // Template Engine
@@ -11,6 +12,11 @@ const bodyParser = require('body-parser');
         app.use(bodyParser.urlencoded({extended: false}))
         app.use(bodyParser.json())
 // Rotas
+    
+    app.get('/', function(req, res) {
+        res.render('home')
+    })
+
     app.get('/cad', function(req, res) {
         // res.send('Rota de cadastro de posts')
         res.render('formulario')
@@ -18,7 +24,16 @@ const bodyParser = require('body-parser');
 
     app.post('/add', function(req, res) {
         // res.send('Formulário recebido!')
-        res.send('Texto: ' + req.body.titulo + " conteudo: " + req.body.conteudo)
+        // res.send('Texto: ' + req.body.titulo + " conteudo: " + req.body.conteudo)
+        Post.create({
+            titulo: req.body.titulo,
+            conteudo: req.body.conteudo
+        }).then(function(){
+            // res.send('Post criado com sucesso!')
+            res.redirect('/')
+        }).catch(function(erro){
+            res.send('Houve um erro: ' + erro)
+        })
     })
 
 // app.get("/", function(req, res){
