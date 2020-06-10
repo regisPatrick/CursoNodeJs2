098138b86d427113c065d65adf337af5f57fe08e
regisPatrick <regis86@hotmail.com>
2020-06-10 19:36:06 -0300

    commit 10-06-2020

--- blog-app/app.js
+++ blog-app/app.js
@@ -6,8 +6,23 @@ const app = express()
 const admin = require('./routes/admin')
 const path = require('path')
 const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 // Configuracoes
+// Sessão
+app.use(session({
+    secret: "cursodenode",
+    resave: true,
+    saveUninitialized: true
+}))
+app.use(flash())
+// Middleware
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.error_msg = req.flash('error_msg')
+    next()
+})
 // Body Parser
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
