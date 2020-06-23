4b8e57c04e906071d407dd29f996d435b0400982
regisPatrick <regis86@hotmail.com>
2020-06-22 23:21:57 -0300

    commit 22-06-2020

--- blog-app/routes/admin.js
+++ blog-app/routes/admin.js
@@ -155,6 +155,24 @@ router.post('/postagens/nova', (req, res) => {
 
 })
 
+router.get('/postagens/edit/:id', (req, res) => {
+    
+    Postagem.findOne({_id: req.params.id}).lean().then((postagem) => {
+
+        Categoria.find().lean().then((categorias) => {
+            res.render('admin/editpostagens', {categorias: categorias, postagem: postagem})
+        }).catch((err) => {
+            req.flash('error_msg', 'Houve um erro ao listar as categorias')
+            res.redirect('/admin/postagens')
+        })
+
+    }).catch((err) => {
+        req.flash('error_msg', 'Houve um erro ao carregar o formulário de edição')
+        res.redirect('/admin/postagens')
+    })
+
+})
+
 // router.get('/teste', (req, res) => {
 //     res.send('Isso é um teste')
 // })
