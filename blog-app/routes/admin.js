2a38d1395760ea041a3b030c05cc4a87a4d99114
regisPatrick <regis86@hotmail.com>
2020-06-24 23:30:19 -0300

    commit 24-06-2020 2

--- blog-app/routes/admin.js
+++ blog-app/routes/admin.js
@@ -197,6 +197,17 @@ router.post('/postagem/edit', (req,res) => {
     })
 })
 
+router.get('/postagens/deletar/:id', (req, res) => {
+    // Postagem.remove({_id: req.params.id}).then(() => { // Método remove está depreciado
+    Postagem.deleteOne({_id: req.params.id}).then(() => {  
+        req.flash('success_msg', 'Postagem deletada com sucesso')
+        res.redirect('/admin/postagens')
+    }).catch((err) => {
+        req.flash('error_msg', 'Houve um erro interno')
+        res.redirect('/admin/postagens')
+    })
+})
+
 // router.get('/teste', (req, res) => {
 //     res.send('Isso é um teste')
 // })
