ae7f7a0a76e4f256cd12819b0626989d50d2a0cc
regisPatrick <regis86@hotmail.com>
2020-06-26 00:14:37 -0300

    commit 26-06-2020

--- blog-app/app.js
+++ blog-app/app.js
@@ -51,10 +51,16 @@ app.set('view engine', 'handlebars');
 
 // Rotas
     app.get('/', (req, res) => {
-        Postagem.find(),populate('categoria').sort.lean().then((postagens) => {
-
+        Postagem.find().populate('categoria').sort({data: 'desc'}).lean().then((postagens) => {
+            res.render('index', {postagens: postagens})
+        }).catch((err) => {
+            req.flash('error_msg', 'Houve um erro interno')
+            res.redirect('/404')
         })
-        res.render('index')
+    })
+
+    app.get('/404', (req, res) => {
+        res.send('Erro 404!')
     })
 
     app.get('/posts', (req, res) => {
