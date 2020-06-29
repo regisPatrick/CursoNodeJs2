98ef3e9cef9306c091326251ccf363d61257e35a
regisPatrick <regis86@hotmail.com>
2020-06-28 23:46:24 -0300

    commit 28-06-2020 2

--- blog-app/models/Usuario.js
+++ blog-app/models/Usuario.js
@@ -5,5 +5,15 @@ const Usuario = new Schema({
     nome: {
         type: String,
         required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    senha: {
+        type: String,
+        required: true
     }
 })
+
+mongoose.model('usuarios', Usuario)
