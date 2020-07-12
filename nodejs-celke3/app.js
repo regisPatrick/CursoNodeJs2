49e8db86e680b086fb9894a0383cc2c1dda1d964
regisPatrick <regis86@hotmail.com>
2020-07-12 17:02:27 -0300

    commit 12-07-2020

--- nodejs-celke3/app.js
+++ nodejs-celke3/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res){
-    res.send('Gerenciador Financeiro');
+    // res.send('Gerenciador Financeiro');
+    res.sendFile(__dirname + '/src/index.html');
 });
 
 app.get('/sobre-empresa', (req, res) => {
-    res.send('Página sobre empresa');
+    // res.send('Página sobre empresa');
+    res.sendFile(__dirname + '/src/sobre-empresa.html')
 });
 
 app.get('/blog', (req, res) => {
