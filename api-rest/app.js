a3aa71ef3a0d91c1bdda0da0d7ce8f566d0caab5
regisPatrick <regis86@hotmail.com>
2020-07-26 20:14:40 -0300

    commit 26-07-2020

--- api-rest/app.js
+++ api-rest/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+require('./models/Artigo');
+const Artigo = mongoose.model('artigo');
+
 const app = express();
 
 app.use(express.json());
@@ -19,6 +22,21 @@ app.get('/', (req, res) => {
     return res.json({
         titulo: 'Como criar API'
     });
+});
+
+app.post('/artigo', (req, res) => {
+    // console.log(req.body);
+    // return res.json(req.body);
+    const artigo = Artigo.create(req.body, (err) => {
+        if(err) return res.status(400).json({
+            error: true,
+            message: 'Error: Artigo não foi cadastrado com sucesso!'
+        })
+        return res.status(200).json({
+            error: false,
+            message: 'Artigo cadastrado com sucesso!'
+        })
+    })
 });
 
 app.listen(8080, () => {
--- api-rest/models/Artigo.js
+++ api-rest/models/Artigo.js
@@ -1,5 +1,17 @@
 const mongoose = require('mongoose');
 
 const Artigo = new mongoose.Schema({
-    
-})
+    titulo: {
+        type: String,
+        required: true
+    },
+    conteudo: {
+        type: String,
+        required: true
+    }
+},
+{
+    timestamps: true,
+});
+
+mongoose.model('artigo', Artigo);
