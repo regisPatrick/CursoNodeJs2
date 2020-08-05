dc076cab4b4390ee8f2e1f6569d75a3fedc55d16
regisPatrick <regis86@hotmail.com>
2020-08-05 16:00:48 -0300

    commit 05-08-2020

--- node-str/server.js
+++ node-str/server.js
@@ -23,6 +23,7 @@ app.use('/', route);
 
 server.listen(port);
 server.on('error', onError);
+server.on('listening', onListening);
 console.log('API rodando na porta ' + port);
 
 function normalizePort(val) {
@@ -61,4 +62,12 @@ function onError(error) {
             throw error;    
     }    
 
+}
+
+function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
 }
