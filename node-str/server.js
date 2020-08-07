f700ce87d756e1edeef2985682eafc899e32bdeb
regisPatrick <regis86@hotmail.com>
2020-08-06 23:48:32 -0300

    commit 06-08-2020

--- node-str/server.js
+++ node-str/server.js
@@ -1,73 +1,73 @@
-'use strict'
+// 'use strict'
 
-// console.log('Testando...');
+// // console.log('Testando...');
 
-const http = require('http');
-const debug = require('debug')('nodestr:server');
-const express = require('express');
+// const http = require('http');
+// const debug = require('debug')('nodestr:server');
+// const express = require('express');
 
-const app = express();
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+// const app = express();
+// const port = normalizePort(process.env.PORT || '3000');
+// app.set('port', port);
 
-const server = http.createServer(app);
-const router = express.Router();
+// const server = http.createServer(app);
+// const router = express.Router();
 
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.1"
-    });
-});
-app.use('/', route);
+// const route = router.get('/', (req, res, next) => {
+//     res.status(200).send({
+//         title: "Node Store API",
+//         version: "0.0.1"
+//     });
+// });
+// app.use('/', route);
 
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-console.log('API rodando na porta ' + port);
+// server.listen(port);
+// server.on('error', onError);
+// server.on('listening', onListening);
+// console.log('API rodando na porta ' + port);
 
-function normalizePort(val) {
-    const port = parseInt(val, 10);
+// function normalizePort(val) {
+//     const port = parseInt(val, 10);
 
-    if(isNaN(port)){
-        return val;
-    }
+//     if(isNaN(port)){
+//         return val;
+//     }
 
-    if (port >= 0){
-        return port;
-    }
+//     if (port >= 0){
+//         return port;
+//     }
 
-    return false;
-}
+//     return false;
+// }
 
-function onError(error) {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
+// function onError(error) {
+//     if (error.syscall !== 'listen') {
+//         throw error;
+//     }
 
-    const bind = typeof port === 'string' ?
-        'Pipe ' + port :
-        'Port ' + port;
+//     const bind = typeof port === 'string' ?
+//         'Pipe ' + port :
+//         'Port ' + port;
 
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;    
-    }    
+//     switch (error.code) {
+//         case 'EACCES':
+//             console.error(bind + ' requires elevated privileges');
+//             process.exit(1);
+//             break;
+//         case 'EADDRINUSE':
+//             console.error(bind + ' is already in use');
+//             process.exit(1);
+//             break;
+//         default:
+//             throw error;    
+//     }    
 
-}
+// }
 
-function onListening() {
-    const addr = server.address();
-    const bind = typeof addr === 'string'
-        ? 'pipe ' + addr
-        : 'port ' + addr.port;
-    debug('Listening on ' + bind);
-}
+// function onListening() {
+//     const addr = server.address();
+//     const bind = typeof addr === 'string'
+//         ? 'pipe ' + addr
+//         : 'port ' + addr.port;
+//     debug('Listening on ' + bind);
+// }
