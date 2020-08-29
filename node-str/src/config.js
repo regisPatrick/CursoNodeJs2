56a5dd797d794baaccb07aed432bc43a14ad78b5
regisPatrick <regis86@hotmail.com>
2020-08-28 23:58:23 -0300

    28-08-2020 2

--- node-str/src/config.js
+++ node-str/src/config.js
@@ -1,2 +1,8 @@
 global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 global.EMAIL_TMPL = '<strong>{0}</strong>';
+
+module.exports = {
+    connectionString: 'mongodb+srv://regis:regis123@cluster0.cibx5.mongodb.net/ndstr?retryWrites=true&w=majority',
+    sendGridKey: 'TBD',
+    userImagesBlobConnectionString: 'TBD'
+}
