8f3fc4f2c35061bdd2a8ef5f9fbc147e1a0e4bd3
regisPatrick <regis86@hotmail.com>
2020-08-30 16:46:58 -0300

    commit 30-08-2020

--- node-str/src/config.js
+++ node-str/src/config.js
@@ -1,5 +1,5 @@
 global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
-global.EMAIL_TMPL = '<strong>{0}</strong>';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo ao Node Store!';
 
 module.exports = {
     connectionString: 'mongodb+srv://regis:regis123@cluster0.cibx5.mongodb.net/ndstr?retryWrites=true&w=majority',
