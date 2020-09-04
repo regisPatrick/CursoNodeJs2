f411ee636f42fc34a7a5d2bfe070e7661f01bcad
regisPatrick <regis86@hotmail.com>
2020-09-04 09:28:19 -0300

    commit 04-09-2020

--- node-str/src/controllers/customer-controller.js
+++ node-str/src/controllers/customer-controller.js
@@ -59,6 +59,7 @@ exports.authenticate = async(req, res, next) => {
         }
 
         const token = await authService.generateToken({
+            id: customer._id,
             email: customer.email, 
             name: customer.name
         });
