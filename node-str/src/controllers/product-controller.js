a6c265bd4bca3449beebb4131df75d17707b6f66
regisPatrick <regis86@hotmail.com>
2020-09-06 20:53:16 -0300

    commit 06-09-2020

--- node-str/src/controllers/product-controller.js
+++ node-str/src/controllers/product-controller.js
@@ -127,22 +127,22 @@ exports.post = async(req, res, next) => {
 
     try{
         // Cria o Blob Service
-        const blobSvc = azure.createBlobService(config.containerConnectionString);
+        // const blobSvc = azure.createBlobService(config.containerConnectionString);
 
-        let filename = guid.raw().toSring() + '.jpg';
-        let rawdata = req.body.image;
-        let matches = rawdata.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-        let type = matches[1];
-        let buffer = new Buffer(matches[2], 'base64');
+        // let filename = guid.raw().toSring() + '.jpg';
+        // let rawdata = req.body.image;
+        // let matches = rawdata.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+        // let type = matches[1];
+        // let buffer = new Buffer(matches[2], 'base64');
 
-        // Salva a imagem
-        await blobSvc.createBlockBlobFromText('product-images', filename, buffer, {
-            contentType: type
-        }, function (error, result, response) {
-            if (error) {
-                filename = 'default-product.png'
-            }
-        });
+        // // Salva a imagem
+        // await blobSvc.createBlockBlobFromText('product-images', filename, buffer, {
+        //     contentType: type
+        // }, function (error, result, response) {
+        //     if (error) {
+        //         filename = 'default-product.png'
+        //     }
+        // });
 
         // await repository.create(req.body);
         await repository.create({
