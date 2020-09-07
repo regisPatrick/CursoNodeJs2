e0d5cfeba7e1465d1ff42118dd93395c0c6c9bc3
regisPatrick <regis86@hotmail.com>
2020-09-07 17:29:46 -0300

    commit 07-09-2020

--- node-str/src/app.js
+++ node-str/src/app.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
 // Conecta ao banco
 mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb+srv://regis:regis123@cluster0.cibx5.mongodb.net/ndstr?retryWrites=true&w=majority', {
+// mongoose.connect('mongodb+srv://regis:senha@cluster0.cibx5.mongodb.net/ndstr?retryWrites=true&w=majority', {
 mongoose.connect(config.connectionString, {    
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -32,8 +32,18 @@ const productRoute = require('./routes/product-route');
 const customerRoute = require('./routes/customer-route');
 const orderRoute = require('./routes/order-route');
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({
+    limit: '5mb'
+}));
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Habilita o CORS
+app.use(function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
 app.use('/', indexRoute);
 app.use('/products', productRoute);
