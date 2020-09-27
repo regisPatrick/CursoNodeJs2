a661d345c60a9ff66de6b2b906a2f84f29ff0d75
regisPatrick <regis86@hotmail.com>
2020-09-27 00:19:39 -0300

    commit 27-09-2020

--- serie-node/src/app/controllers/projectController.js
+++ serie-node/src/app/controllers/projectController.js
@@ -9,8 +9,4 @@ router.get('/', (req, res) => {
     res.send({ ok: true, user: req.userId });
 });
 
-router.get('/', (req, res) => {
-    res.send({ ok: true, user: req.userId });
-});
-
 module.exports = app => app.use('/projects', router);
