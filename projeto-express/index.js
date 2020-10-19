2890b14392246458fbdf02edb1e83351bf190e9b
regisPatrick <regis86@hotmail.com>
2020-10-19 01:37:56 -0300

    commit 19-10-2020

--- projeto-express/index.js
+++ projeto-express/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.get('/', function(req, res){
+    res.send("Seja bem-vindo ao meu app!");
+});
 
+app.get('/sobre', function(req, res){
+    res.send('Minha página sobre');
+});
+
+app.get('/blog', function(req, res){
+    res.send('Bem vindo ao meu blog!');
+});
 
 app.listen(8081, function(){
     console.log("Servidor Rodando na url http://localhost:8081");
