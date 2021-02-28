dc560a992c083bb57e36bf3401d5923bed370940
regisPatrick <regis86@hotmail.com>
2021-02-28 19:06:26 -0300

    commit 28-02-2021

---
+++ teste3/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const handlebars = require('express-handlebars');
+const Sequelize = require('sequelize')
+
+// Config
+    // Template Engine
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        app.set('view engine', 'handlebars')
+    // Conexão com o banco de dados MySql
+        const sequelize = new Sequelize('test', 'root', 'root', {
+            host: "localhost",
+            dialect: 'mysql'
+        })
+
+
+app.listen(8081, function(){
+    console.log("Servidor rodando na url http://localhost:8081");
+});
